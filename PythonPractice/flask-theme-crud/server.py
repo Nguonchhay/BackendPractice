6cbbe53cd8431a35ba7a4d23103f989209ba5ee6
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-08-02 19:11:26 +0700

    [FLASK-THEME-CRUD] Login and Logout with session

--- PythonPractice/flask-theme-crud/server.py
+++ PythonPractice/flask-theme-crud/server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, session, redirect
 
 
 app = Flask(__name__)
+app.secret_key = "12345678"
+
 
 @app.route("/")
 def homePage():
@@ -12,11 +14,33 @@ def homePage():
 def aboutPage():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contactPage():
     return render_template("contact.html")
 
 
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        session["user"] = {
+            "email": email,
+            "name": "Fake User"
+        }
+        print(session["user"])
+        return redirect(url_for("homePage"))
+    else:
+        return render_template("auth/login.html")
+
+
+@app.route("/logout", methods = ["POST"])
+def logout():
+    session["user"] = {}
+    return redirect(url_for("homePage"))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
     
