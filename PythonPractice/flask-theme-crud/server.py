fdf8146fc691578208e2e5f259d3c0497297ad3e
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-08-02 22:33:16 +0700

    [FLASK-THEME-CRUD] Permanent session

--- PythonPractice/flask-theme-crud/server.py
+++ PythonPractice/flask-theme-crud/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, url_for, session, redirect
+from datetime import timedelta
 
 
 app = Flask(__name__)
 app.secret_key = "12345678"
+app.permanent_session_lifetime = timedelta(days = 1)
 
 
 @app.route("/")
@@ -25,6 +27,8 @@ def login():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
+        
+        session.permanent = True
         session["user"] = {
             "email": email,
             "name": "Fake User"
@@ -32,12 +36,14 @@ def login():
         print(session["user"])
         return redirect(url_for("homePage"))
     else:
+        if "user" in session and "email" in session["user"]:
+            return redirect(url_for("homePage"))
         return render_template("auth/login.html")
 
 
 @app.route("/logout", methods = ["POST"])
 def logout():
-    session["user"] = {}
+    session.pop("user", None)
     return redirect(url_for("homePage"))
 
 
