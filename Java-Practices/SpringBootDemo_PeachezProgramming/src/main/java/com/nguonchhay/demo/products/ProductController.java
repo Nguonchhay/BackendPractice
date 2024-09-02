0e5e52f12a70bb5e2c70b639e9f2e26783afd503
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2024-09-02 16:16:02 +0700

    [SpringBoot] REST product - create, update, delete with basic

--- Java-Practices/SpringBootDemo_PeachezProgramming/src/main/java/com/nguonchhay/demo/products/ProductController.java
+++ Java-Practices/SpringBootDemo_PeachezProgramming/src/main/java/com/nguonchhay/demo/products/ProductController.java
@@ -3,10 +3,7 @@ package com.nguonchhay.demo.products;
 import com.nguonchhay.demo.products.Models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +23,24 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Product>> getProduct(@PathVariable Integer id) {
         return ResponseEntity.ok(productRepository.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+        product.setId(id);
+        productRepository.save(product);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable Integer id) {
+        productRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
