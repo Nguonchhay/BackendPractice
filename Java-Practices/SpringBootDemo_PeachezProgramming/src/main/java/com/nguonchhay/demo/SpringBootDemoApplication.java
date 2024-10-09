02221ecacdc931a6eddb694d8ec86667936e650b
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2024-10-09 16:39:44 +0700

    [SpringBoot] REST - cache manager and schedule

--- Java-Practices/SpringBootDemo_PeachezProgramming/src/main/java/com/nguonchhay/demo/SpringBootDemoApplication.java
+++ Java-Practices/SpringBootDemo_PeachezProgramming/src/main/java/com/nguonchhay/demo/SpringBootDemoApplication.java
@@ -2,10 +2,8 @@ package com.nguonchhay.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
