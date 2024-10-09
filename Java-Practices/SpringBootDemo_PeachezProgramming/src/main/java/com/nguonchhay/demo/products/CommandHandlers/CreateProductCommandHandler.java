688b6e0587ad075833c422352615abdddfdc46c0
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2024-10-09 17:01:40 +0700

    [SpringBoot] Logger with default - SLF4J

--- Java-Practices/SpringBootDemo_PeachezProgramming/src/main/java/com/nguonchhay/demo/products/CommandHandlers/CreateProductCommandHandler.java
+++ Java-Practices/SpringBootDemo_PeachezProgramming/src/main/java/com/nguonchhay/demo/products/CommandHandlers/CreateProductCommandHandler.java
@@ -7,6 +7,8 @@ import com.nguonchhay.demo.products.Models.Category;
 import com.nguonchhay.demo.products.Models.Product;
 import com.nguonchhay.demo.products.ProductRepository;
 import com.nguonchhay.demo.products.Requests.ProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,8 +22,11 @@ public class CreateProductCommandHandler implements Command<ProductRequest, Resp
     @Autowired
     private CategoryRepository categoryRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(CreateProductCommandHandler.class);
+
     @Override
     public ResponseEntity execute(ProductRequest productRequest) {
+        logger.info("Executing " + getClass() + " with " + productRequest.toString());
         Product product = validateProduct(productRequest);
         productRepository.save(product);
         return ResponseEntity.ok(product);
@@ -32,14 +37,17 @@ public class CreateProductCommandHandler implements Command<ProductRequest, Resp
                 .orElseThrow(() -> new ProductNotValidateException("Category id is required"));
 
         if (productRequest.getName().isEmpty()) {
+            logger.error("Product name is required");
             throw new ProductNotValidateException("Product name is required");
         }
 
         if (productRequest.getPrice() <= 0.0) {
+            logger.error("Price is required");
             throw new ProductNotValidateException("Price is required");
         }
 
         if (productRequest.getQuantity() <= 0) {
+            logger.error("Quantity is required");
             throw new ProductNotValidateException("Quantity is required");
         }
 
